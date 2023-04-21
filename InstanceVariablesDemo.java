432b4061061c5436b8b75c0a5bcc6eda476f58dd
K-Venkata-Kiran <kondeti.venkatakiran2001@gmail.com>
2023-04-21 19:48:57 +0530

    added InstanceVariablesDemo.java

---
+++ InstanceVariablesDemo.java
@@ -0,0 +1,11 @@
+public class InstanceVariablesDemo {
+   int age = 21;
+   String name = "Kiran";
+   String email = "kondeti.venkatakiran2001@gmail.com";
+    public static void main(String[] args) {
+        InstanceVariablesDemo obj1 = new InstanceVariablesDemo();
+        System.out.println(obj1.age);
+        System.out.println(obj1.name);
+        System.out.println(obj1.email);
+    }
+}
